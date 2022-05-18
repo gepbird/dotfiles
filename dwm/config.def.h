3cff152b0de5f6687ccfb716235491b46b07b9c9
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2022-05-18 22:47:16 +0200

    Configure dwm: change shortcuts, rules

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -2,9 +2,9 @@
 
 /* appearance */
 static const unsigned int borderpx = 1;        /* border pixel of windows */
-static const unsigned int snap = 32;       /* snap pixel */
-static const int showbar = 1;        /* 0 means no bar */
-static const int topbar = 1;        /* 0 means bottom bar */
+static const unsigned int snap = 32;           /* snap pixel */
+static const int showbar = 1;                  /* 0 means no bar */
+static const int topbar = 1;                   /* 0 means bottom bar */
 static const char* fonts[] = { "monospace:size=10" };
 static const char dmenufont[] = "monospace:size=10";
 static const char col_gray1[] = "#222222";
@@ -26,9 +26,11 @@ static const Rule rules[] = {
    *	WM_CLASS(STRING) = instance, class
    *	WM_NAME(STRING) = title
    */
-   /* class      instance    title       tags mask     isfloating   monitor */
-   { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-   { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+   /* class                       instance    title       tags mask     isfloating   monitor */
+   { "discord"                   ,NULL       ,NULL       ,1 << 0       ,0           ,1 },
+   { "Microsoft Teams - Preview" ,NULL       ,NULL       ,1 << 1       ,0           ,1 },
+   { "Chromium"                  ,NULL       ,NULL       ,1 << 2       ,0           ,1 },
+   { "flameshot"                 ,NULL       ,NULL       ,0            ,1           ,-1 },
 };
 
 /* layout(s) */
@@ -46,73 +48,80 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
-#define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
-/* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define Alt MODKEY
+#define Control ControlMask
+#define Shift ShiftMask
+#define Super Mod4Mask
+
+#define TAGKEYS(KEY,TAG) \
+	{ Super                         ,KEY      ,view           ,{.ui = 1 << TAG} }, \
+	{ Super | Control               ,KEY      ,toggleview     ,{.ui = 1 << TAG} }, \
+	{ Super | Shift                 ,KEY      ,tag            ,{.ui = 1 << TAG} }, \
+	{ Super | Control | Shift       ,KEY      ,toggletag      ,{.ui = 1 << TAG} },
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char* dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char* termcmd[] = { "st", NULL };
+static const char* termcmd[] = { "alacritty", "-e", "fish", NULL };
+static const char* flameshotfull[] = { "flameshot", "full", NULL };
+static const char* flameshotgui[] = { "flameshot", "gui", NULL };
+static const char* flameshotguidelayed[] = { "flameshot", "gui", "-d", "2500", NULL };
 
 static Key keys[] = {
-  /* modifier                     key        function        argument */
-  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-  { MODKEY | ShiftMask,           XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_b,      togglebar,      {0} },
-  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-  { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-  { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-  { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-  { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-  { MODKEY,                       XK_Return, zoom,           {0} },
-  { MODKEY,                       XK_Tab,    view,           {0} },
-  { MODKEY | ShiftMask,           XK_c,      killclient,     {0} },
-  { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-  { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-  { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-  { MODKEY,                       XK_space,  setlayout,      {0} },
-  { MODKEY | ShiftMask,           XK_space,  togglefloating, {0} },
-  { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-  { MODKEY | ShiftMask,           XK_0,      tag,            {.ui = ~0 } },
-  { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-  { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-  { MODKEY | ShiftMask,           XK_comma,  tagmon,         {.i = -1 } },
-  { MODKEY | ShiftMask,           XK_period, tagmon,         {.i = +1 } },
-  TAGKEYS(XK_1,                      0)
-  TAGKEYS(XK_2,                      1)
-  TAGKEYS(XK_3,                      2)
-  TAGKEYS(XK_4,                      3)
-  TAGKEYS(XK_5,                      4)
-  TAGKEYS(XK_6,                      5)
-  TAGKEYS(XK_7,                      6)
-  TAGKEYS(XK_8,                      7)
-  TAGKEYS(XK_9,                      8)
-  {
- MODKEY | ShiftMask,             XK_q,      quit,           {0}
-},
+  /* modifier                      key       function        argument */
+  {  Super                        ,XK_p     ,spawn          ,{.v = dmenucmd } },
+  {  Super                        ,XK_Return,spawn          ,{.v = termcmd } },
+  {  0                            ,XK_Print ,spawn          ,{.v = flameshotfull} },
+  {  Control                      ,XK_Print ,spawn          ,{.v = flameshotgui } },
+  {  Control | Shift              ,XK_Print ,spawn          ,{.v = flameshotguidelayed } },
+  {  Super                        ,XK_b     ,togglebar      ,{0} },
+  {  Super                        ,XK_j     ,focusstack     ,{.i = +1 } },
+  {  Super                        ,XK_k     ,focusstack     ,{.i = -1 } },
+  {  Super                        ,XK_i     ,incnmaster     ,{.i = +1 } },
+  {  Super                        ,XK_d     ,incnmaster     ,{.i = -1 } },
+  {  Super                        ,XK_h     ,setmfact       ,{.f = -0.05} },
+  {  Super                        ,XK_l     ,setmfact       ,{.f = +0.05} },
+  {  Super                        ,XK_Tab   ,zoom           ,{0} },
+  {  Alt                          ,XK_Tab   ,view           ,{0} },
+  {  Super                        ,XK_q     ,killclient     ,{0} },
+  {  Super                        ,XK_t     ,setlayout      ,{.v = &layouts[0]} },
+  {  Super                        ,XK_f     ,setlayout      ,{.v = &layouts[1]} },
+  {  Super                        ,XK_m     ,setlayout      ,{.v = &layouts[2]} },
+  {  Super                        ,XK_space ,setlayout      ,{0} },
+  {  Super | Shift                ,XK_space ,togglefloating ,{0} },
+  {  Super                        ,XK_0     ,view           ,{.ui = ~0 } },
+  {  Super | Shift                ,XK_0     ,tag            ,{.ui = ~0 } },
+  {  Super                        ,XK_comma ,focusmon       ,{.i = -1 } },
+  {  Super                        ,XK_period,focusmon       ,{.i = +1 } },
+  {  Super | Shift                ,XK_comma ,tagmon         ,{.i = -1 } },
+  {  Super | Shift                ,XK_period,tagmon         ,{.i = +1 } },
+  {  Super | Shift                ,XK_q     ,quit           ,{0} },
+  TAGKEYS(XK_1, 0)
+  TAGKEYS(XK_2, 1)
+  TAGKEYS(XK_3, 2)
+  TAGKEYS(XK_4, 3)
+  TAGKEYS(XK_5, 4)
+  TAGKEYS(XK_6, 5)
+  TAGKEYS(XK_7, 6)
+  TAGKEYS(XK_8, 7)
+  TAGKEYS(XK_9, 8)
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
   /* click                event mask      button          function        argument */
-  { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-  { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-  { ClkWinTitle,          0,              Button2,        zoom,           {0} },
-  { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-  { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-  { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-  { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-  { ClkTagBar,            0,              Button1,        view,           {0} },
-  { ClkTagBar,            0,              Button3,        toggleview,     {0} },
-  { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-  { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+  { ClkLtSymbol          ,0              ,Button1        ,setlayout      ,{0} },
+  { ClkLtSymbol          ,0              ,Button3        ,setlayout      ,{.v = &layouts[2]} },
+  { ClkWinTitle          ,0              ,Button2        ,zoom           ,{0} },
+  { ClkStatusText        ,0              ,Button2        ,spawn          ,{.v = termcmd } },
+  { ClkClientWin         ,Super          ,Button1        ,movemouse      ,{0}, },
+  { ClkClientWin         ,Super          ,Button2        ,togglefloating ,{0}, },
+  { ClkClientWin         ,Super          ,Button3        ,resizemouse    ,{0}, },
+  { ClkTagBar            ,0              ,Button1        ,view           ,{0} },
+  { ClkTagBar            ,0              ,Button3        ,toggleview     ,{0} },
+  { ClkTagBar            ,Super          ,Button1        ,tag            ,{0}, },
+  { ClkTagBar            ,Super          ,Button3        ,toggletag      ,{0}, },
 };
 
--- dwm/dwm.c
+++ dwm/dwm.c
@@ -280,7 +280,7 @@ static Monitor* mons, * selmon;
 static Window root, wmcheckwin;
 
 /* configuration, allows nested code to access above variables */
-#include "config.h"
+#include "config.def.h"
 
 /* compile-time check if all tags fit into an unsigned int bit array. */
 struct NumTags { char limitexceeded[LENGTH(tags) > 31 ? -1 : 1]; };
