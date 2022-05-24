96c8c521b620c413ff7c32d20b6df1908d3fac88
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2022-05-24 21:29:16 +0200

    Add adjacenttag patch to dwm

--- home/.dwm/dwm.c
+++ home/.dwm/dwm.c
@@ -189,8 +189,10 @@ static void maprequest(XEvent* e);
 static void monocle(Monitor* m);
 static void motionnotify(XEvent* e);
 static void movemouse(const Arg* arg);
+static unsigned int nexttag(void);
 static Client* nexttiled(Client* c);
 static void pop(Client*);
+static unsigned int prevtag(void);
 static void propertynotify(XEvent* e);
 static void quit(const Arg* arg);
 static Monitor* recttomon(int x, int y, int w, int h);
@@ -215,6 +217,8 @@ static void sigchld(int unused);
 static void spawn(const Arg* arg);
 static void tag(const Arg* arg);
 static void tagmon(const Arg* arg);
+static void tagtonext(const Arg* arg);
+static void tagtoprev(const Arg* arg);
 static void tile(Monitor*);
 static void togglebar(const Arg* arg);
 static void togglefloating(const Arg* arg);
@@ -234,6 +238,8 @@ static void updatetitle(Client* c);
 static void updatewindowtype(Client* c);
 static void updatewmhints(Client* c);
 static void view(const Arg* arg);
+static void viewnext(const Arg* arg);
+static void viewprev(const Arg* arg);
 static Client* wintoclient(Window w);
 static Monitor* wintomon(Window w);
 static int xerror(Display* dpy, XErrorEvent* ee);
@@ -1220,11 +1226,25 @@ movemouse(const Arg* arg)
   }
 }
 
+unsigned int
+nexttag(void)
+{
+  unsigned int seltag = selmon->tagset[selmon->seltags];
+  return seltag == (1 << (LENGTH(tags) - 1)) ? 1 : seltag << 1;
+}
+
 Client*
 nexttiled(Client* c)
 {
   for(; c && (c->isfloating || !ISVISIBLE(c)); c = c->next);
   return c;
+}
+
+unsigned int
+prevtag(void)
+{
+  unsigned int seltag = selmon->tagset[selmon->seltags];
+  return seltag == 1 ? (1 << (LENGTH(tags) - 1)) : seltag >> 1;
 }
 
 void
@@ -1779,6 +1799,32 @@ tagmon(const Arg* arg)
 }
 
 void
+tagtonext(const Arg* arg)
+{
+  unsigned int tmp;
+
+  if(selmon->sel == NULL)
+    return;
+
+  tmp = nexttag();
+  tag(&(const Arg) { .ui = tmp });
+  view(&(const Arg) { .ui = tmp });
+}
+
+void
+tagtoprev(const Arg* arg)
+{
+  unsigned int tmp;
+
+  if(selmon->sel == NULL)
+    return;
+
+  tmp = prevtag();
+  tag(&(const Arg) { .ui = tmp });
+  view(&(const Arg) { .ui = tmp });
+}
+
+void
 tile(Monitor* m)
 {
   unsigned int i, n, h, mw, my, ty;
@@ -2165,6 +2211,18 @@ view(const Arg* arg)
     selmon->tagset[selmon->seltags] = arg->ui & TAGMASK;
   focus(NULL);
   arrange(selmon);
+}
+
+void
+viewnext(const Arg* arg)
+{
+  view(&(const Arg) { .ui = nexttag() });
+}
+
+void
+viewprev(const Arg* arg)
+{
+  view(&(const Arg) { .ui = prevtag() });
 }
 
 Client*
