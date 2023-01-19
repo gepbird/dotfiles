5bccf373fee9330c90fdabd6d64d72f5749b05d9
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-01-19 18:54:11 +0100

    Remove chaotic aur

--- preinstall.sh
+++ preinstall.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if ! grep -Fq '[chaotic-aur]' /etc/pacman.conf; then
-  sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-  sudo pacman-key --lsign-key FBA220DFC880C036
-  sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-  echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
-fi
-
 sudo pacman -Syy
 sudo pacman -S --noconfirm --needed community/ttf-iosevka-nerd # starship dependency
 sudo pacman -S --noconfirm --needed community/starship         # custom prompt
