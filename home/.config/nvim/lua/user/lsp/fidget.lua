d558eed15049e62cea13275d0c4dc90ae8be3f6d
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-06-23 12:09:35 +0200

    Add fidget.nvim

---
+++ home/.config/nvim/lua/user/lsp/fidget.lua
@@ -0,0 +1,50 @@
+require 'fidget'.setup {
+  text = {
+    spinner = 'pipe',
+    done = '✔',
+    commenced = 'Started',
+    completed = 'Completed',
+  },
+  align = {
+    bottom = true,
+    right = true,
+  },
+  timer = {
+    spinner_rate = 125,
+    fidget_decay = 2000,
+    task_decay = 1000,
+  },
+  window = {
+    relative = 'win',
+    blend = 100,
+    zindex = nil,
+    border = 'none',
+  },
+  fmt = {
+    leftpad = true,
+    stack_upwards = true,
+    max_width = 0,
+    fidget =
+        function(fidget_name, spinner)
+          return string.format('%s %s', spinner, fidget_name)
+        end,
+    task =
+        function(task_name, message, percentage)
+          return string.format(
+            '%s%s [%s]',
+            message,
+            percentage and string.format(' (%s%%)', percentage) or '',
+            task_name
+          )
+        end,
+  },
+  sources = {
+    ['*'] = {
+      ignore = false,
+    },
+  },
+  debug = {
+    logging = false,
+    strict = false,
+  },
+}
