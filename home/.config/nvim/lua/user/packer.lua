c45add1a4d9fe999b95227062737d6400edcdc5a
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-07-14 13:35:31 +0200

    Auto install packer

--- home/.config/nvim/lua/user/packer.lua
+++ home/.config/nvim/lua/user/packer.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+    vim.cmd [[ packadd packer.nvim ]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 local packer = require 'packer'
 packer.startup(function(use)
   use { 'wbthomason/packer.nvim' }
@@ -75,6 +88,10 @@ packer.startup(function(use)
   use { 'moll/vim-bbye' }
   use { 'github/copilot.vim', config = function() require 'user.copilot' end }
 end)
+
+if packer_bootstrap then
+  packer.sync()
+end
 
 require 'user.utils'.register_maps {
   { 'n', '<space>pi', function()
