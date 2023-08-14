51bf1fc633c187155cf134b0024995d86d07f1c1
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-08-14 18:55:15 +0200

    Add atuin

--- home.nix
+++ home.nix
@@ -110,6 +110,21 @@ in
         };
       };
     }
+    {
+      programs.atuin = {
+        enable = true;
+        flags = [ "--disable-up-arrow" ];
+        settings = {
+          auto_sync = false;
+          update_check = false;
+          search_mode = "skim";
+          inline_height = 10;
+          show_preview = true;
+          show_help = false;
+          exit_mode = "return-query";
+        };
+      };
+    }
   ];
 
   home-manager.users.gep = {
