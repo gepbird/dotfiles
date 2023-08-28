5255d4ed19571e159616db6c69312c03e77d184b
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-08-28 17:47:30 +0200

    Update dwm from 6.3 to 6.4+master, switch to rofi
    Start with a fresh latest dwm (v6.4+81f17d). Remove centeredmaster
    patch. Update custom patches: changeborder, focuscursor and resizehere.
    Extract custom configs into patches: rofi, oneconfig.
    Remake config.def.h: formatting is more similar to upstream's, macro for
    making scratchpads.

--- home/.local/share/dwm/util.h
+++ home/.local/share/dwm/util.h
@@ -4,16 +4,5 @@
 #define MIN(A, B)               ((A) < (B) ? (A) : (B))
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 
-#define lprint(text) { \
-  FILE* fp = fopen("/tmp/dwm.log", "a"); \
-  fprintf(fp, text); \
-  fclose(fp); \
-}
-#define lprintf(text, arg0) { \
-  FILE* fp = fopen("/tmp/dwm.log", "a"); \
-  fprintf(fp, text, arg0); \
-  fclose(fp); \
-}
-
 void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
