f0bbdebbc5ba2815bbd0c43cc294dee7d6db368d
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-10-14 18:10:17 +0200

    Move power management settings to only geptop
    This fixes keyboard and mouse going into sleep mode after ~5s of
    inactivity.

--- system.nix
+++ system.nix
@@ -16,23 +16,6 @@
 
   hardware.opentabletdriver.enable = true;
 
-  # hopefully more battery time with these settings
-  powerManagement = {
-    cpuFreqGovernor = "schedutil";
-    powertop.enable = true;
-  };
-  services.power-profiles-daemon.enable = false;
-  services.thermald.enable = true;
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 0;
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-    };
-  };
-
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/Budapest";
