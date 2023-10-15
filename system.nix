de95313cdb60946393c105cfc86321f6970ba7cb
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-10-15 16:46:37 +0200

    Host ssh server, only allow login from my devices

--- system.nix
+++ system.nix
@@ -21,6 +21,23 @@
 
   networking.networkmanager.enable = true;
 
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+  users.users.gep.openssh.authorizedKeys.keys = [
+    # geptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDPWyW2tBcCORf4C/Z7iPaKGoiswyLdds3m8ZrNY8OXl gutyina.gergo.2@gmail.com"
+    # geppc
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGW5zKjn01DVf6vTs/D2VV+/awXTNboY1iaCThi2A1v gep@geppc"
+    # gepphone
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQXysKutq2b67RAmq46qMH8TDLEYf0D5SYon4vE6efO u0_a483@localhost"
+  ];
+
   time.timeZone = "Europe/Budapest";
 
   console.useXkbConfig = true;
