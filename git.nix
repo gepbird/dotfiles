d19c0facef2eb33c70108d0569911d56e076e4b1
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-10-30 15:17:13 +0100

    Git: clone recursivile

--- git.nix
+++ git.nix
@@ -14,8 +14,8 @@
         d = "diff";
         ds = "diff --staged";
         co = "checkout";
-        cl = "clone";
-        gh = "!git clone git@github.com:$1";
+        cl = "clone --recursive";
+        gh = "!git clone --recursive git@github.com:$1";
         p = "!git pull && git push";
         l = "log --pretty=format:'%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(dim green)(%cr) [%an]' --abbrev-commit -30";
         churl = "remote set-url origin";
