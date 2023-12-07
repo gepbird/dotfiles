fa607c1e1aac1ab269ebc090bebb7e233c89ce4b
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-12-07 22:19:26 +0100

    Lazy load lsp configs

--- nvim/lua/gep/lsp/init.lua
+++ nvim/lua/gep/lsp/init.lua
@@ -22,39 +22,55 @@ local lspconfig = require 'lspconfig'
 local utils = require 'gep.utils'
 
 local servers = {
-  'lua_ls',
-  'pyright',
-  'omnisharp',
-  'clangd',
-  'cssls',
-  'emmet_ls',
-  'tsserver',
-  'html',
-  'jsonls',
-  'phpactor',
-  'rnix',
-  'texlab',
-  'lemminx',
-  'taplo',
-  'yamlls',
+  lua_ls = { 'lua' },
+  pyright = { 'python' },
+  omnisharp = { 'cs' },
+  clangd = { 'c' },
+  cssls = { 'css' },
+  emmet_ls = { 'html' },
+  tsserver = { 'typescript', 'javascript' },
+  html = { 'html' },
+  jsonls = { 'json' },
+  phpactor = { 'php' },
+  rnix = { 'nix' },
+  texlab = { 'tex' },
+  lemminx = { 'xml' },
+  taplo = { 'toml' },
+  yamlls = { 'yaml' },
 }
-for _, server in ipairs(servers) do
-  local ok, config = pcall(require, 'gep.lsp.' .. server)
-  if not ok then
-    config = {}
+for server, languages in pairs(servers) do
+  for _, language in ipairs(languages) do
+    -- lazy load servers
+    utils.register_autocmd {
+      'FileType',
+      function()
+        -- don't load it multiple times
+        if not servers[server] then
+          return
+        end
+        servers[server] = nil
+
+        local ok, config = pcall(require, 'gep.lsp.' .. server)
+        if not ok then
+          config = {}
+        end
+        lspconfig[server].setup(config)
+        vim.cmd [[:LspStart]]
+      end,
+      { pattern = language },
+    }
   end
-  lspconfig[server].setup(config)
 end
 
 local lsp = vim.lsp.buf
 local telescope = require 'telescope.builtin'
 local ivy = require 'telescope.themes'.get_ivy()
 utils.register_maps {
-  { 'n', '<space>ls',    ':LspInfo<cr>' },
-  { 'n', '<space>-',     function() telescope.lsp_references(ivy) end },
-  { 'n', '<space>.',     vim.lsp.buf.definition }, -- omnisharp-extended doesn't work with telescope definitions
-  { 'n', '<space>:',     function() telescope.lsp_type_definitions(ivy) end },
-  { 'n', '<space>f',     function()
+  { 'n', '<space>ls', ':LspInfo<cr>' },
+  { 'n', '<space>-',  function() telescope.lsp_references(ivy) end },
+  { 'n', '<space>.',  vim.lsp.buf.definition }, -- omnisharp-extended doesn't work with telescope definitions
+  { 'n', '<space>:',  function() telescope.lsp_type_definitions(ivy) end },
+  { 'n', '<space>f', function()
     lsp.format { async = false };
     vim.cmd ':w'
   end },
