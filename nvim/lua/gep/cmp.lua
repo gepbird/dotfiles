517eeac03811fb8d58cf9516264e584f7e19f29f
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2023-12-10 15:04:34 +0100

    Simplify cmp formatting

--- nvim/lua/gep/cmp.lua
+++ nvim/lua/gep/cmp.lua
@@ -33,7 +33,7 @@ local kind_icons = {
   TypeParameter = '',
   Copilot = '',
 }
---
+
 local check_backspace = function()
   local col = vim.fn.col '.' - 1
   return col == 0 or vim.fn.getline '.':sub(col, col):match '%s'
@@ -85,15 +85,8 @@ cmp.setup {
   formatting = {
     fields = { 'kind', 'abbr', 'menu' },
     format = function(entry, vim_item)
-      vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
-      --vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-      vim_item.menu = ({
-        nvim_lsp = '[LSP]',
-        nvim_lua = '[NVIM_LUA]',
-        luasnip = '[Snippet]',
-        buffer = '[Buffer]',
-        path = '[Path]',
-      })[entry.source.name]
+      vim_item.kind = kind_icons[vim_item.kind]
+      vim_item.menu = '[' .. entry.source.name .. ']'
       return vim_item
     end,
   },
