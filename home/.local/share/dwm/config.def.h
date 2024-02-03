443db0ed0893f0b5d1152bbf37457ca6c5db2e6a
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-02-03 01:37:26 +0100

    Swap monitors again

--- home/.local/share/dwm/config.def.h
+++ home/.local/share/dwm/config.def.h
@@ -40,8 +40,8 @@ static const Rule rules[] = {
    *  WM_NAME(STRING) = title
    */
   /* class       instance    title       tags mask     isfloating   monitor */
-  { "discord",   NULL,       NULL,       1 << 0,       0,           0  },
-  { "firefox",   NULL,       NULL,       1 << 1,       0,           0  },
+  { "discord",   NULL,       NULL,       1 << 0,       0,           1  },
+  { "firefox",   NULL,       NULL,       1 << 1,       0,           1  },
   { "flameshot", NULL,       NULL,       0,            1,           -1 },
   { NULL,        NULL,       "spterm",   SPTAG(0),     1,           -1 },
   { NULL,        NULL,       "spclac",   SPTAG(1),     1,           -1 },
