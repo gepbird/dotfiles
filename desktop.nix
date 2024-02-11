26775fba2dfe4a183d15634b95fceb9f55df00e5
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-02-11 10:34:51 +0100

    Make sound better for osu: enable alsa, disable pipewire workaround
    BASS faulted with error code 4 and 46 still happens, but sometimes
    it's possible to play other audio when osu is open sometimes.

--- desktop.nix
+++ desktop.nix
@@ -61,19 +61,21 @@
   users.users.gep.extraGroups = [ "video" ];
 
   services.pipewire = {
+    alsa.enable = true; # required for osu!
     enable = true;
     pulse.enable = true;
   };
   # make less audio stutters when high gpu+cpu usage by buffering and delaying audio by ~30ms 
-  environment.etc = {
-    "pipewire/pipewire.conf.d/90-bigger-buffer.conf".text = ''
-      context.properties = {
-        default.clock.rate = 48000
-        default.clock.min-quantum = 1500
-        default.clock.max-quantum = 1500
-      }
-    '';
-  };
+  # disabled because it causes audio desync in osu!
+  #environment.etc = {
+  #  "pipewire/pipewire.conf.d/90-bigger-buffer.conf".text = ''
+  #    context.properties = {
+  #      default.clock.rate = 48000
+  #      default.clock.min-quantum = 1500
+  #      default.clock.max-quantum = 1500
+  #    }
+  #  '';
+  #};
 
   systemd.user.services = {
     polkit-gnome-authentication-agent-1 = {
