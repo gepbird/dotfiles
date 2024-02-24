7355a4821feb3f22bce692b696ffdca951408333
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-02-24 11:38:21 +0100

    Use hm for development.nix

--- development.nix
+++ development.nix
@@ -1,82 +1,80 @@
 { self, config, pkgs, ... }:
 
 {
-  home-manager.users.gep = {
-    xdg.mimeApps.defaultApplications = {
-      "text/plain" = [ "nvim.desktop" ];
-    };
+  hm.programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraLuaConfig = "require 'gep'";
+    plugins = with pkgs; with vimUtils;
+      with pkgs.vimPlugins; [
+        nvim-web-devicons
 
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      extraLuaConfig = "require 'gep'";
-      plugins = with pkgs; with vimUtils;
-        with pkgs.vimPlugins; [
-          nvim-web-devicons
+        vscode-nvim
+        lualine-nvim
+        bufferline-nvim
+        nvim-window-picker
+        neo-tree-nvim
+        toggleterm-nvim
+        nvim-bqf
+        undotree
+        vim-bbye
 
-          vscode-nvim
-          lualine-nvim
-          bufferline-nvim
-          nvim-window-picker
-          neo-tree-nvim
-          toggleterm-nvim
-          nvim-bqf
-          undotree
-          vim-bbye
+        vim-repeat
+        vim-sandwich
+        comment-nvim
+        nvim-autopairs
+        nvim-lastplace
 
-          vim-repeat
-          vim-sandwich
-          comment-nvim
-          nvim-autopairs
-          nvim-lastplace
+        nvim-treesitter.withAllGrammars
+        nvim-treesitter-textobjects
+        rainbow-delimiters-nvim
+        nvim-colorizer-lua
 
-          nvim-treesitter.withAllGrammars
-          nvim-treesitter-textobjects
-          rainbow-delimiters-nvim
-          nvim-colorizer-lua
+        telescope-nvim
+        telescope-ui-select-nvim
+        telescope-fzf-native-nvim
 
-          telescope-nvim
-          telescope-ui-select-nvim
-          telescope-fzf-native-nvim
+        vim-fugitive
+        vim-rhubarb
+        gitsigns-nvim
 
-          vim-fugitive
-          vim-rhubarb
-          gitsigns-nvim
+        nvim-cmp
+        cmp-nvim-lsp
+        cmp-buffer
+        cmp-path
+        cmp-cmdline
+        cmp-nvim-lua
+        cmp_luasnip
+        copilot-lua
+        copilot-cmp
+        luasnip
+        friendly-snippets
 
-          nvim-cmp
-          cmp-nvim-lsp
-          cmp-buffer
-          cmp-path
-          cmp-cmdline
-          cmp-nvim-lua
-          cmp_luasnip
-          copilot-lua
-          copilot-cmp
-          luasnip
-          friendly-snippets
+        nvim-lspconfig
+        guard-nvim
+        lspsaga-nvim
+        trouble-nvim
+        fidget-nvim
 
-          nvim-lspconfig
-          guard-nvim
-          lspsaga-nvim
-          trouble-nvim
-          fidget-nvim
+        nvim-dap
+        nvim-dap-ui
+        nvim-dap-virtual-text
+        telescope-dap-nvim
 
-          nvim-dap
-          nvim-dap-ui
-          nvim-dap-virtual-text
-          telescope-dap-nvim
+        rust-tools-nvim
+        flutter-tools-nvim
+        markdown-preview-nvim
+        omnisharp-extended-lsp-nvim
+        vimtex
+      ];
+  };
 
-          rust-tools-nvim
-          flutter-tools-nvim
-          markdown-preview-nvim
-          omnisharp-extended-lsp-nvim
-          vimtex
-        ];
-    };
+  hm.home.file = {
+    ".config/nvim/lua".source =
+      self.lib.mkDotfilesSymlink config "nvim/lua";
+  };
 
-    home.file = {
-      ".config/nvim/lua".source =
-        self.lib.mkDotfilesSymlink config "nvim/lua";
-    };
+  hm.xdg.mimeApps.defaultApplications = {
+    "text/plain" = [ "nvim.desktop" ];
   };
 }
