25f1472852d535d3a5e285b798b7f4f5ca0e3f36
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-03-11 00:11:18 +0100

    Remove deprecated option in gitsigns

--- modules/nvim/lua/gep/gitsigns.lua
+++ modules/nvim/lua/gep/gitsigns.lua
@@ -13,7 +13,6 @@ gs.setup {
   linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
   word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
-    interval = 1000,
     follow_files = true,
   },
   attach_to_untracked = true,
