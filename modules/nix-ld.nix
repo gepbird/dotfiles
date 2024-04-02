afc343394a062ffe751bc4d4d5908a0255fd9143
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-04-02 12:00:03 +0200

    Add electron deps to nix-ld

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -3,5 +3,30 @@ self: { pkgs, ... }:
 {
   programs.nix-ld = {
     enable = true;
+    libraries = with pkgs;
+      let
+        electron-deps = [
+          glib
+          nss
+          nspr
+          atk
+          cups
+          dbus
+          xorg_sys_opengl
+          gtk3
+          pango
+          cairo
+          xorg.libX11
+          xorg.libXcomposite
+          xorg.libXdamage
+          xorg.libXfixes
+          xorg.libXrandr
+          mesa
+          xorg.libxcb
+          libxkbcommon
+          alsa-lib
+        ];
+      in
+      electron-deps;
   };
 }
