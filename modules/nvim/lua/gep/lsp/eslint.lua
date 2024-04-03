7e274b2871dd2e3fbce9017cee744c31f9849756
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-04-03 22:43:58 +0200

    Disable EslintFixAll

--- modules/nvim/lua/gep/lsp/eslint.lua
+++ modules/nvim/lua/gep/lsp/eslint.lua
@@ -1,9 +1,11 @@
-require 'gep.utils'.register_maps {
-  { 'n', '<space>f', function()
-    vim.cmd ':EslintFixAll'
-    vim.lsp.buf.format { async = false }
-    vim.cmd ':w'
-  end },
-}
+-- disabled for now, because after it runs, it tries to format every buffer which don't have eslint attached to it
+-- TODO: re-enable this
+--require 'gep.utils'.register_maps {
+--  { 'n', '<space>f', function()
+--    vim.cmd ':EslintFixAll'
+--    vim.lsp.buf.format { async = false }
+--    vim.cmd ':w'
+--  end },
+--}
 
 return {}
