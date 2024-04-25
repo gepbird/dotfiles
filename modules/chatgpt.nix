9ee8f02fb5413c0c45eda932a82f96832a41c303
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-04-25 19:00:12 +0200

    chatgpt: inject token at activation time rather than with readFile

--- modules/chatgpt.nix
+++ modules/chatgpt.nix
@@ -1,15 +1,25 @@
-self: { config, pkgs, ... }:
+self: { config, pkgs, lib, ... }:
 
 let
-  jsonFormat = pkgs.formats.json { };
-in
-{
-  hm-gep.home.packages = [ pkgs.chatgpt-cli ];
-
-  hm-gep.xdg.configFile."chatgpt/config.json".source = jsonFormat.generate "config.json" {
-    api_key = builtins.readFile config.age.secrets.openai-token.path;
+  configuration = {
+    api_key = "@secret@";
     conversation = {
       model = "gpt-4-turbo";
     };
   };
+  runConfigPath = "/run/chatgpt-config.json";
+in
+{
+  hm-gep.home.packages = [ pkgs.chatgpt-cli ];
+
+  hm-gep.xdg.configFile."chatgpt/config.json".source =
+    config.hm-gep.lib.file.mkOutOfStoreSymlink runConfigPath;
+
+  system.activationScripts."chatgpt-secret" = ''
+    secret=$(cat ${config.age.secrets.openai-token.path})
+    echo '${builtins.toJSON configuration}' \
+      | ${lib.getExe pkgs.gnused} "s#@secret@#$secret#" \
+      > ${runConfigPath}
+    chmod 400 ${runConfigPath}
+  '';
 }
