518680bda87a4b727e07d42505cf6c695e9b7aa1
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-05-09 12:45:02 +0200

    Add workaround for flameshot not starting due to tray.target

--- modules/hm.nix
+++ modules/hm.nix
@@ -10,5 +10,14 @@ self: { config, lib, home-manager, ... }:
 
   home-manager.users.gep = {
     home.stateVersion = config.system.stateVersion;
+
+    # TODO: remove when fixed: https://github.com/nix-community/home-manager/issues/2064
+    # workaround for 'Failed to restart flameshot.service: Unit tray.target not found.'
+    systemd.user.targets.tray = {
+      Unit = {
+        Description = "Home Manager System Tray";
+        Requires = [ "graphical-session-pre.target" ];
+      };
+    };
   };
 }
