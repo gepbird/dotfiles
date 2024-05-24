6c30a46915466af165106c6bc3aa4121d36ce7ea
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-05-25 01:19:28 +0200

    Fix stylelint incorrectly linting in ts files

--- modules/nvim/lua/gep/lsp/stylelint_lsp.lua
+++ modules/nvim/lua/gep/lsp/stylelint_lsp.lua
@@ -1,6 +1,15 @@
 return {
   cmd = {
     'stylelint-lsp',
-    '--stdio'
-  }
+    '--stdio',
+  },
+  -- TODO: remove when fixed: https://github.com/bmatcuk/stylelint-lsp/issues/39
+  filetypes = {
+    'css',
+    'less',
+    'scss',
+    'sugarss',
+    'vue',
+    'wxss',
+  },
 }
