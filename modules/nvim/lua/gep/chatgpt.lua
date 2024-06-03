0975bbf52a195c3db19ef2d25ef3d1116289ab7b
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-06-03 22:15:09 +0200

    Add nvim keybind to open chatgpt

--- modules/nvim/lua/gep/chatgpt.lua
+++ modules/nvim/lua/gep/chatgpt.lua
@@ -1,3 +1,9 @@
-require'chatgpt'.setup {
+local chatgpt = require 'chatgpt'
+
+chatgpt.setup {
   api_key_cmd = 'cat /run/openai-token',
+}
+
+require 'gep.utils'.register_maps {
+  { 'n', '<space>ő', chatgpt.openChat },
 }
