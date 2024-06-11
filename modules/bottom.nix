d651a4eea7494c6258b02cbd422d685fd56d9d48
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-06-12 01:07:30 +0200

    bottom: customize layout

--- modules/bottom.nix
+++ modules/bottom.nix
@@ -6,7 +6,64 @@ self: { ... }:
     settings = {
       flags = {
         color = "gruvbox";
+        enable_gpu = true;
+        default_time_value = "30s";
       };
+      processes.columns = [
+        "name"
+        "mem%"
+        "cpu%"
+        # TODO: this will be introduced in 0.9.6+
+        #"gmem%"
+        #"gpu%"
+        "pid"
+        "read"
+        "write"
+        "tread"
+        "twrite"
+        "state"
+        "user"
+        "time"
+      ];
+      row = [
+        {
+          ratio = 40;
+          child = [
+            {
+              type = "mem";
+            }
+            {
+              type = "cpu";
+            }
+          ];
+        }
+        {
+          ratio = 30;
+          child = [
+            {
+              ratio = 30;
+              type = "temp";
+            }
+            {
+              ratio = 70;
+              type = "disk";
+            }
+          ];
+        }
+        {
+          ratio = 30;
+          child = [
+            {
+              ratio = 40;
+              type = "net";
+            }
+            {
+              ratio = 60;
+              type = "proc";
+            }
+          ];
+        }
+      ];
     };
   };
 }
