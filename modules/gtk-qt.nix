43988b6ef36352d2be2f810e3434a4b93aad5f83
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-07-04 12:06:21 +0200

    refactor(gtk): change gtk4/libadwaita dark theme hack

--- modules/gtk-qt.nix
+++ modules/gtk-qt.nix
@@ -12,12 +12,13 @@ self: { config, pkgs, ... }:
     };
   };
 
-  # gtk4 dark theme
-  hm-gep.xdg.configFile."gtk-4.0/gtk.css".source =
-    config.hm-gep.lib.file.mkOutOfStoreSymlink
-      # this gtk4 css tries to import gtk3/libadwaita.css that breaks some styles
-      # I deliberately didn't link that gtk3 css, so it won't be loaded and won't break styles
-      "${pkgs.adw-gtk3}/share/themes/adw-gtk3-dark/gtk-4.0/gtk.css";
+  # dark theme for libadwaita (mostly/only gtk4?)
+  environment.sessionVariables = {
+    ADW_DISABLE_PORTAL = 1; # use color-scheme from dconf rather than from portal
+  };
+  hm-gep.dconf.settings = {
+    "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  };
 
   qt = {
     enable = true;
