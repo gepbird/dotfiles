5b1d659c0b483def388ae8fd0c50c88ff183da02
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-07-09 09:39:19 +0200

    nvidia: load config on startup

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,4 +1,4 @@
-self: { pkgs, ... }:
+self: { config, pkgs, lib, ... }:
 
 {
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -11,4 +11,25 @@ self: { pkgs, ... }:
   hm-gep.home.packages = with pkgs; [
     nvtopPackages.full
   ];
+
+  systemd.user.services.nvidia-settings = {
+    description = "nvidia-settings";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig =
+      let
+        nvidiaSettings = lib.getExe config.hardware.nvidia.package.settings;
+        settingsFile = pkgs.writeText ".nvidia-settings-rc" ''
+          # fix colors being too dark
+          [DPY:DVI-D-0]/ColorRange=1
+          [DPY:HDMI-0]/ColorRange=1
+          [DPY:DP-1]/ColorRange=1
+        '';
+      in
+      {
+        Type = "oneshot";
+        ExecStart = "${nvidiaSettings} --config=${settingsFile} --load-config-only";
+      };
+  };
 }
