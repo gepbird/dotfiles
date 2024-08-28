338b1027edcc8dd78fdfd274824b8d6afb756d81
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-08-28 10:38:36 +0200

    nvim: remove rarely used snippets

--- modules/nvim/lua/gep/snippets/init.lua
+++ modules/nvim/lua/gep/snippets/init.lua
@@ -1,18 +1,7 @@
-local ls = require 'luasnip'
+local luasnip = require 'luasnip'
 
-ls.config.set_config {
+luasnip.config.set_config {
   enable_autosnippets = true,
 }
 
-local languages = {
-  'lua',
-  'cs',
-  'dart',
-  'java',
-  'tex',
-}
-for _, language in ipairs(languages) do
-  ls.add_snippets(language, require('gep.snippets.' .. language), { type = 'autosnippets' })
-end
-
-require 'luasnip.loaders.from_vscode'.lazy_load()
+luasnip.add_snippets('tex', require 'gep.snippets.tex', { type = 'autosnippets' })
