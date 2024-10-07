db3765acd18a4e0862691620b4833a01dbb5819c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-10-08 01:07:32 +0200

    ente-auth: init

---
+++ modules/ente-auth.nix
@@ -0,0 +1,9 @@
+self: { pkgs, ... }:
+
+{
+  # TODO: fix conflict with rustdesk-flutter
+  #hm-gep.home.packages = [ pkgs.ente-auth ];
+  environment.systemPackages = [ pkgs.ente-auth ];
+
+  services.gnome.gnome-keyring.enable = true;
+}
