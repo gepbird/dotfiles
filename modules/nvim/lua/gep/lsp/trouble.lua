ea7b68926dc1a4eee78bc2f3e478c7d3e851c332
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-11-21 14:44:50 +0100

    nvim: remove merged vscode.nvim patch

--- modules/nvim/lua/gep/lsp/trouble.lua
+++ modules/nvim/lua/gep/lsp/trouble.lua
@@ -6,7 +6,3 @@ trouble.setup {
 require 'gep.utils'.register_maps {
   { 'n', '<space>,', function() trouble.toggle('diagnostics') end },
 }
-
--- TODO: remove when merged: https://github.com/Mofiqul/vscode.nvim/pull/203
-vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
-vim.api.nvim_set_hl(0, 'TroubleNormalNC', { link = 'Normal' })
