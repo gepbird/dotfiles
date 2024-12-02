08f3288b8bc33df22b7b40c8709e5e84f184a655
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-12-02 18:54:33 +0100

    lix: remove merged patch for nixd

--- flake.nix
+++ flake.nix
@@ -36,9 +36,7 @@
       inputs.git-hooks.follows = "git-hooks";
     };
     lix-module = {
-      # TODO: remove patch after merged: https://git.lix.systems/lix-project/nixos-module/pulls/46
-      #url = "https://git.lix.systems/lix-project/nixos-module/archive/stable.tar.gz";
-      url = "https://git.lix.systems/gepbird/lix-nixos-module/archive/stable.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/stable.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
