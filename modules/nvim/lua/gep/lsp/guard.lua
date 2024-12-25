98e3fc77331a87362730f96de16ddf81446eecdb
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2024-12-25 17:25:09 +0100

    nvim: remove yapf config from guard as pylsp can format

--- modules/nvim/lua/gep/lsp/guard.lua
+++ modules/nvim/lua/gep/lsp/guard.lua
@@ -2,8 +2,6 @@ local ft = require 'guard.filetype'
 -- TODO: uncomment when added back https://github.com/gepbird/guard-collection/commit/03107f58d3224bb6b8dee30f13edd776cdab8ba4
 --ft 'javascript,typescript,css,json,yaml,markdown':fmt 'prettierd'
 ft 'css,yaml,markdown':fmt 'prettier'
-ft 'python':fmt 'yapf'
-  :extra('--style', '{indent_width: 2}')
 vim.g.guard_config = {
   fmt_on_save = true,
   lsp_as_default_formatter = false,
