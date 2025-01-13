601d2612e94e927591eababfd66f0e879bf7ebd4
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-01-13 16:54:43 +0100

    ente-auth: remove patch

--- modules/ente-auth.nix
+++ modules/ente-auth.nix
@@ -4,17 +4,8 @@ self:
   ...
 }:
 
-let
-  # TODO: remove when fixed: https://github.com/ente-io/ente/issues/4536
-  ente-auth = pkgs.ente-auth.overrideAttrs (o: {
-    prePatch = ''
-      substituteInPlace lib/utils/directory_utils.dart \
-        --replace-fail "return cacheHome" "return Directory(p.join(cacheHome.path, 'enteauthinit'));"
-    '';
-  });
-in
 {
-  hm-gep.home.packages = [ ente-auth ];
+  hm-gep.home.packages = [ pkgs.ente-auth ];
 
   services.gnome.gnome-keyring.enable = true;
 }
