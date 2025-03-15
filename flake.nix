b2e83dec0f3abe7a89bb39110576ca89fbce7fcf
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-03-15 11:01:43 +0100

    firefox: remove overlay

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
       url = "github:NixOS/nixpkgs/nixos-unstable";
       #url = "/home/gep/nixpkgs";
     };
-    # TODO: remove after https://github.com/NixOS/nixpkgs/pull/387931
-    nixpkgs-firefox = {
-      url = "github:NixOS/nixpkgs/b58e19b11fe72175fd7a9e014a4786a91e99da5f";
-    };
     # TODO: remove after https://github.com/NixOS/nixpkgs/issues/389638
     nixpkgs-rustdesk = {
       url = "github:NixOS/nixpkgs/e3e32b642a31e6714ec1b712de8c91a3352ce7e1";
