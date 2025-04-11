ab9cba457d7c18553cfc810cae18fb3d5db4280f
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-04-11 23:19:56 +0200

    secrets: include all secrets in secret.nix automatically

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,24 @@
 let
+  inherit (builtins)
+    attrNames
+    listToAttrs
+    map
+    readDir
+    removeAttrs
+    toString
+    ;
+
   ssh-module = import ../modules/ssh.nix null { };
   keys = ssh-module.users.users.gep.openssh.authorizedKeys.keys;
+
+  secretFiles = attrNames (removeAttrs (readDir (toString ./.)) [ "secrets.nix" ]);
+  result = listToAttrs (
+    map (x: {
+      name = x;
+      value = {
+        publicKeys = keys;
+      };
+    }) secretFiles
+  );
 in
-{
-  "system-password.age".publicKeys = keys;
-  "openai-token.age".publicKeys = keys;
-}
+result
