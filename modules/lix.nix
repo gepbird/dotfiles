0522ffc7ac20c359d1b2c8fcaefc1dd91a5f3a2b
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-04-20 23:49:33 +0200

    lix: use latest version from nixpkgs, shadow the warning

--- modules/lix.nix
+++ modules/lix.nix
@@ -7,4 +7,10 @@ self:
   imports = [
     self.inputs.lix-module.nixosModules.lixFromNixpkgs
   ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      lix = prev.lixPackageSets.latest.lix;
+    })
+  ];
 }
