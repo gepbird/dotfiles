356420c73a96de25b136152bffcdd00177bc86db
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-05-12 22:45:43 +0200

    chatgpt: remove chatgpt-cli, keep API key

--- modules/chatgpt.nix
+++ modules/chatgpt.nix
@@ -1,34 +1,10 @@
 self:
 {
   config,
-  pkgs,
-  lib,
   ...
 }:
 
-let
-  configuration = {
-    api_key = "@secret@";
-    conversation = {
-      model = "gpt-4-turbo";
-    };
-  };
-  runConfigPath = "/run/chatgpt-config.json";
-in
 {
-  hm-gep.home.packages = [ pkgs.chatgpt-cli ];
-
-  hm-gep.xdg.configFile."chatgpt/config.json".source = config.hm-gep.lib.file.mkOutOfStoreSymlink runConfigPath;
-
-  system.activationScripts."chatgpt-secret" = ''
-    secret=$(cat ${config.age.secrets.openai-token.path})
-    echo '${builtins.toJSON configuration}' \
-      | ${lib.getExe pkgs.gnused} "s#@secret@#$secret#" \
-      > ${runConfigPath}
-    chown ${config.users.users.gep.name} ${runConfigPath}
-    chmod 400 ${runConfigPath}
-  '';
-
   age.secrets = {
     openai-token.owner = config.users.users.gep.name;
   };
