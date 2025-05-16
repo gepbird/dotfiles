a7929bfc4aa469877e1e418f76cfe52a17b4f3a1
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-05-16 10:10:05 +0200

    flake: allow patching nixpkgs

--- flake.nix
+++ flake.nix
@@ -113,9 +113,28 @@
       lib = import ./lib.nix { };
       nixosConfigurations =
         let
+          # TODO: don't hardcode system
+          # maybe try to wrap it in a function (that is used to replace nixosSystem)
+          # and get nixpkgs.hostPlatform from the config?
+          system = "x86_64-linux";
+          pkgs = import nixpkgs { inherit system; };
+          # take "nixpkgs" input as a base and apply patches that start with "nixpkgs-patch"
+          patches = builtins.attrValues (
+            nixpkgs.lib.filterAttrs (n: v: builtins.match "^nixpkgs-patch.*" n != null) inputs
+          );
+          patchedNixpkgs = pkgs.applyPatches {
+            name = "nixpkgs-patched";
+            src = nixpkgs;
+            inherit patches;
+          };
+          # don't use the patchedNixpkgs without patches, it takes time to build it
+          finalNixpkgs = if patches == [ ] then nixpkgs else patchedNixpkgs;
+          nixosSystem = import "${finalNixpkgs}/nixos/lib/eval-config.nix";
+
           mkSystem =
             host:
-            nixpkgs.lib.nixosSystem {
+            nixosSystem {
+              inherit system;
               modules = [ host ];
               specialArgs = inputs;
             };
