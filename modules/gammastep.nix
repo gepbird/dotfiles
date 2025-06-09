b0c500d465a4db0d87de7d26a399f395e7ed2fdb
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-06-10 01:36:28 +0200

    Revert "gammastep: remove geoclue2 as it failed to get location"
    This reverts commit f47d487f50fddf3cb2c982e1a62ed6596258cde8.
    
    Previously it used retired Mozilla Location Services,
    but now it uses beaconDB.

--- modules/gammastep.nix
+++ modules/gammastep.nix
@@ -4,13 +4,14 @@ self:
 }:
 
 {
+  services.geoclue2.enable = true;
+
   hm-gep.services.gammastep = {
+    provider = "geoclue2";
     enable = true;
     temperature = {
       day = 4000;
       night = 2700;
     };
-    latitude = 42.52;
-    longitude = 27.36;
   };
 }
