1662fcffc7bdd724a2b21a99031106c607303642
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-06-18 00:52:17 +0200

    nvim/blink: configure most of the plugin

--- modules/nvim/lua/gep/blink-cmp.lua
+++ modules/nvim/lua/gep/blink-cmp.lua
@@ -1,2 +1,56 @@
+-- TODO: set up lsp capabilities
 require 'blink-cmp'.setup {
+  keymap = {
+    preset = 'none',
+    ['<c-space>'] = { 'show' },
+    ['<c-j>'] = { 'select_next' },
+    ['<c-k>'] = { 'select_prev' },
+    ['<c-l>'] = { 'accept' },
+    ['<a-j>'] = { 'scroll_documentation_down' },
+    ['<a-k>'] = { 'scroll_documentation_up' },
+  },
+  completion = {
+    menu = {
+      draw = {
+        columns = {
+          { 'kind_icon' },
+          { 'label',    'label_description', gap = 1 },
+          { 'source_id' },
+        },
+      },
+    },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 0,
+    },
+    ghost_text = {
+      enabled = true,
+    },
+  },
+  signature = {
+    enabled = true,
+  },
+  sources = {
+    providers = {
+      lsp = {
+        fallbacks = {},
+      },
+    },
+  },
+  cmdline = {
+    keymap = {
+      preset = 'none',
+      ['<c-space>'] = { 'show' },
+      ['<c-j>'] = { 'select_next' },
+      ['<c-k>'] = { 'select_prev' },
+      ['<c-l>'] = { 'accept' },
+      ['<a-j>'] = { 'scroll_documentation_down' },
+      ['<a-k>'] = { 'scroll_documentation_up' },
+    },
+    completion = {
+      menu = {
+        auto_show = true,
+      },
+    },
+  },
 }
