62cde3b5af80c086e631984f51abc266e225edb5
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-08-08 11:23:57 +0200

    earlyoom: make more agressive, hopefully it prevents next system freeze

--- modules/earlyoom.nix
+++ modules/earlyoom.nix
@@ -6,5 +6,10 @@ self:
 {
   services.earlyoom = {
     enable = true;
+    reportInterval = 60;
+    freeSwapThreshold = 20;
+    freeMemThreshold = 20;
+    freeSwapKillThreshold = 10;
+    freeMemKillThreshold = 15;
   };
 }
