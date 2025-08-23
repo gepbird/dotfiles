2dc7dc06bf2221704c8255676b144248f8492a63
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-08-23 23:04:12 +0200

    nix: nvd -> dix

--- modules/nix.nix
+++ modules/nix.nix
@@ -35,6 +35,7 @@ in
     with pkgs;
     [
       cachix
+      dix
       hydra-check
       nix-diff
       nix-inspect
@@ -45,7 +46,6 @@ in
       nixd
       nixfmt
       nixpkgs-review
-      nvd
     ]
     ++ aliasFunctionPackages;
 
