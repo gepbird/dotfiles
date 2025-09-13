971c77c3a5f4dbeb112837553bfb5275d295c75d
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-09-13 12:02:11 +0200

    nvim: use generic nvim path instead of /nix/store path

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,30 +1,27 @@
 self:
 {
   pkgs,
-  lib,
   ...
 }:
 
 let
   package = self.inputs.nvim.packages.${pkgs.system}.default;
-  binary = lib.getExe package;
 in
 {
   hm-gep.home.packages = [
     package
   ];
 
-  hm-gep.home.sessionVariables.EDITOR = binary;
-
   hm-gep.xdg.mimeApps.defaultApplications = {
     "text/plain" = [ "nvim.desktop" ];
   };
 
   hm-gep.home.shellAliases = {
-    v = binary;
+    v = "nvim";
   };
 
   hm-gep.home.sessionVariables = {
-    MANPAGER = "${binary} +Man!";
+    EDITOR = "nvim";
+    MANPAGER = "nvim +Man!";
   };
 }
