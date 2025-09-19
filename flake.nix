80b12ccd38577007d906279ee0d1bbb20805e961
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-09-19 09:16:35 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/f35703b412c67b48e97beb6e27a6ab96a084cd37' (2025-09-07)
      → 'github:nix-community/home-manager/363007f12930caf8b0ea59c0bf5be109c52ad0ef' (2025-09-19)
    • Updated input 'nix-index-database':
        'github:nix-community/nix-index-database/9b144dc3ef6e42b888c4190e02746aab13b0e97f' (2025-09-07)
      → 'github:nix-community/nix-index-database/050a5feb5d1bb5b6e5fc04a7d3d816923a87c9ea' (2025-09-14)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/8eb28adfa3dc4de28e792e3bf49fcf9007ca8ac9' (2025-09-05)
      → 'github:NixOS/nixpkgs/0147c2f1d54b30b5dd6d4a8c8542e8d7edf93b5d' (2025-09-18)
    • Removed input 'nixpkgs-patch-rnote-0-13-0'
    • Updated input 'nur':
        'github:nix-community/NUR/33dc71f0bc337f655063b814f874c64553415937' (2025-09-07)
      → 'github:nix-community/NUR/f1ee8889e667e4c206fdfcf0de45ccb32fd4568c' (2025-09-19)
    • Updated input 'nvim':
        'github:gepbird/nvim/e5c34da926a8aed8fabbe1d5c23e8b2de4608f94' (2025-09-06)
      → 'github:gepbird/nvim/21c8bfd4f2514112518dc0b2753e61d73abce952' (2025-09-15)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/1aabc6c05ccbcbf4a635fb7a90400e44282f61c4' (2025-08-31)
      → 'github:numtide/treefmt-nix/128222dc911b8e2e18939537bed1762b7f3a04aa' (2025-09-18)

--- flake.nix
+++ flake.nix
@@ -17,10 +17,6 @@
       url = "github:gepbird/nixpkgs-patcher";
       #url = "/home/gep/nixpkgs-patcher";
     };
-    nixpkgs-patch-rnote-0-13-0 = {
-      url = "https://github.com/NixOS/nixpkgs/pull/440811.diff";
-      flake = false;
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
