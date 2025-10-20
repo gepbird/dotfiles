506e96e29f98be00dfbf7a44f9bd50a6cbeb9f4d
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-10-20 08:28:34 +0200

    dwm: remove gromit-mpx patch

--- modules/dwm.nix
+++ modules/dwm.nix
@@ -7,25 +7,6 @@ self:
 {
   services.xserver.windowManager.dwm = {
     enable = true;
-    # TODO: remove override in next nixos-unstable
-    # this pulls packages from the current, patched nixpkgs to fix gromit-mpx build failure
-    package = self.inputs.dwm-gep.packages.${pkgs.system}.default.overrideAttrs {
-      buildInputs =
-        pkgs.dwm.buildInputs
-        ++ (with pkgs; [
-          bottom
-          clac
-          ente-auth
-          flameshot
-          gnused
-          gromit-mpx
-          lf
-          rofi
-          wrapGAppsHook
-          xfce.xfce4-terminal
-          xorg.xkill
-          zsh
-        ]);
-    };
+    package = self.inputs.dwm-gep.packages.${pkgs.system}.default;
   };
 }
