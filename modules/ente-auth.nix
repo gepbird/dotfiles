2616822a889298d91ed962b95204c33adc4d5f2c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-10-24 12:31:47 +0200

    ente-auth: remove gnome-keyring caching, as other packages override it

--- modules/ente-auth.nix
+++ modules/ente-auth.nix
@@ -14,7 +14,6 @@ self:
   services.gnome.gnome-keyring.enable = true;
 
   nixpkgs.overlays = [
-    (self.lib.maybeCachePackageOverlay self "gnome-keyring")
     (self.lib.maybeCachePackageOverlay self "grc")
   ];
 }
