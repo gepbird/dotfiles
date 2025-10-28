c4637c365a4f3dfd7846b812065250b1d992d9f9
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-10-28 22:42:30 +0100

    Reapply "gammastep: remove geoclue2 as it failed to get location"
    This reverts commit b0c500d465a4db0d87de7d26a399f395e7ed2fdb.
    
    BeaconDB API is timing out.

--- modules/gammastep.nix
+++ modules/gammastep.nix
@@ -5,17 +5,14 @@ self:
 }:
 
 {
-  services.geoclue2 = {
-    enable = true;
-  };
-
   hm-gep.services.gammastep = {
     enable = true;
     package = self.lib.maybeCachePackage self pkgs.gammastep;
-    provider = "geoclue2";
     temperature = {
       day = 4000;
       night = 2700;
     };
+    latitude = 42.52;
+    longitude = 27.36;
   };
 }
