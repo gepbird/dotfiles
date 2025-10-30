74c0b1e8b81a413af6941b00888ceb2650986082
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-10-30 11:20:36 +0100

    Revert "Reapply "gammastep: remove geoclue2 as it failed to get location""
    This reverts commit c4637c365a4f3dfd7846b812065250b1d992d9f9.
    
    The issue was caused by the hosting provider, it was fixed.

--- modules/gammastep.nix
+++ modules/gammastep.nix
@@ -5,14 +5,17 @@ self:
 }:
 
 {
+  services.geoclue2 = {
+    enable = true;
+  };
+
   hm-gep.services.gammastep = {
     enable = true;
     package = self.lib.maybeCachePackage self pkgs.gammastep;
+    provider = "geoclue2";
     temperature = {
       day = 4000;
       night = 2700;
     };
-    latitude = 42.52;
-    longitude = 27.36;
   };
 }
