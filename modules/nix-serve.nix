2d0b037defd3522b4f3a2152d7e073ef8622b084
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-17 11:39:39 +0100

    nix-serve: move proxy logic to raspi-dotfiles, new cache

--- modules/nix-serve.nix
+++ modules/nix-serve.nix
@@ -19,55 +19,24 @@ in
 
   nix.settings = {
     substituters = [
-      #"https://cache.gepbird.ovh"
-      "http://localhost:${toString proxyStorePort}"
+      # uses cache.gepbird.ovh safely (without failing if it is offline)
+      # https://github.com/tchfoo/raspi-dotfiles/commit/53406a458478f227f5fd0810684a39aff098dfa7
+      "https://nix-cache.tchfoo.com" 
     ];
     trusted-public-keys = [
-      "cache.gepbird.ovh-1:/2wN3bgUOBBp2e2a7Rj2hieqdyL0fEoLF0ohldEtMEY="
-    ];
-    trusted-users = [
-      "gep"
+      "nix-cache.tchfoo.com-1:pWK4l0phRA3bE0CviZodEQ5mWAQYoiuVi2LML+VNtNY="
     ];
     secret-key-files = [
-      config.age.secrets."cache.gepbord.ovh-1.priv".path
+      config.age.secrets."nix-cache.tchfoo.com-1.sec".path
     ];
   };
 
-  # expose the current nix store to cache.gepbird.ovh
   services.nginx.virtualHosts."cache.gepbird.ovh" = lib.mkIf enableServing {
     enableACME = true;
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://localhost:${toString config.services.nix-serve.port}";
       recommendedProxySettings = true;
-    };
-  };
-
-  # a proxy store only locally exposed, it
-  # - returns cache.gepbird.ovh if it is online
-  # - returns a dummy nix store if it is offline
-  services.nginx.virtualHosts."cache.gepbird.ovh-proxy" = {
-    listen = [
-      {
-        addr = "localhost";
-        port = proxyStorePort;
-      }
-    ];
-    locations = {
-      "/nix-cache-info".extraConfig = ''
-        return 200 'StoreDir: /nix/store\nWantMassQuery: 1\nPriority: 30\n';
-      '';
-      "/" = {
-        proxyPass = "https://cache.gepbird.ovh";
-        recommendedProxySettings = true;
-        extraConfig = ''
-          proxy_connect_timeout 500ms;
-          error_page 502 504 = @fallback;
-        '';
-      };
-      "@fallback".extraConfig = ''
-        try_files _ =404;
-      '';
     };
   };
 }
