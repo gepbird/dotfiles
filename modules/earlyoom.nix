0d4ef8aaa79f7ac3c790141a900125809ec2fb39
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-12-16 02:09:48 +0100

    earlyoom: update thresholds, add more processes

--- modules/earlyoom.nix
+++ modules/earlyoom.nix
@@ -9,18 +9,18 @@ self:
     enable = true;
     package = self.lib.maybeCachePackage self pkgs.earlyoom;
     reportInterval = 60;
-    freeSwapThreshold = 20;
-    freeMemThreshold = 20;
-    freeSwapKillThreshold = 10;
-    freeMemKillThreshold = 15;
+    freeSwapThreshold = 15;
+    freeMemThreshold = 15;
+    freeSwapKillThreshold = 5;
+    freeMemKillThreshold = 10;
     # regex patterns match against values in /proc/<PID>/comm, which is truncated at 15 bytes
     extraArgs = [
       "--prefer"
-      "(electron|systemd-coredum|Discord|Isolated Web|Web Content|nixd|clangd|OmniSharp|rust-analyzer)"
+      "(electron|systemd-coredum|Discord|Isolated Web|Web Content|WebExtensions|nixd|nil|clangd|OmniSharp|rust-analyzer)"
       "--avoid"
-      "(gammastep|dunst|OpenTablet|gromit-mpx)"
+      "(OpenTablet|gromit-mpx|.flameshot-wrap|.dwm-status-wra|picom)"
       "--ignore"
-      "(wireplumber|pipewire|pipewire-pulse|dbus-daemon)"
+      "(wireplumber|pipewire|pipewire-pulse|dbus-daemon|.gcr-ssh-agent-|ssh-agent|.agent-wrapped|gammastep|dunst|xss-lock|dconf-service|systemd)"
     ];
   };
 }
