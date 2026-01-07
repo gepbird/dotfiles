dd39073bacba13fa4c51481276a55fbbe490e3e1
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-01-07 16:24:30 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/a34fae9c08a15ad73f295041fec82323541400a9' (2025-12-15)
      → 'github:hercules-ci/flake-parts/250481aafeb741edfe23d29195671c19b36b6dca' (2026-01-05)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/1cfa305fba94468f665de1bd1b62dddf2e0cb012' (2026-01-03)
      → 'github:nix-community/home-manager/aea57993a89bfc2a66c0434e0f4383ebf164e2a3' (2026-01-07)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/fb7944c166a3b630f177938e478f0378e64ce108' (2026-01-02)
      → 'github:NixOS/nixpkgs/9f0c42f8bc7151b8e7e5840fb3bd454ad850d8c5' (2026-01-05)
    • Removed input 'nixpkgs-patch-grayjay-fix-build'
    • Updated input 'nur':
        'github:nix-community/NUR/ed59c24d452493c9c0d3d297c1b6c0412d353b4c' (2026-01-03)
      → 'github:nix-community/NUR/69678dba7e0ec92d9f13e5904d499efc7b3f641c' (2026-01-07)
    • Updated input 'nvim-gep':
        'git+https://git.tchfoo.com/gepbird/nvim?ref=refs/heads/main&rev=c00a8b55a2608a200bbdbd4e7449f428fcf71966' (2026-01-03)
      → 'git+https://git.tchfoo.com/gepbird/nvim?ref=refs/heads/main&rev=3a72ea96675698f78b60fe516441759909d018b2' (2026-01-05)
    • Updated input 'sops-nix':
        'github:Mic92/sops-nix/61b39c7b657081c2adc91b75dd3ad8a91d6f07a7' (2025-12-28)
      → 'github:Mic92/sops-nix/ecc41505948ec2ab0325f14c9862a4329c2b4190' (2026-01-04)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/d56486eb9493ad9c4777c65932618e9c2d0468fc' (2026-01-03)
      → 'github:numtide/treefmt-nix/4db0238d79254c6d14f251808dc5264b8fc81b73' (2026-01-06)

--- flake.nix
+++ flake.nix
@@ -10,10 +10,6 @@
       url = "github:gepbird/nixpkgs-patcher";
       #url = "/home/gep/nixpkgs-patcher";
     };
-    nixpkgs-patch-grayjay-fix-build = {
-      url = "https://github.com/NixOS/nixpkgs/pull/475903.diff";
-      flake = false;
-    };
     nixpkgs-patch-nvim-treesitter-legacy = {
       url = "https://github.com/NixOS/nixpkgs/pull/472119.diff";
       flake = false;
