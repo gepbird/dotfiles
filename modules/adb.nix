898bd688fee62e98b107725eddfa3dd049b12e43
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-01-07 16:22:46 +0100

    adb: refactor
    Remove redundant android-udev-tools as of systemd 258 and inline the
    module as it will be dropped in the next update.

--- modules/adb.nix
+++ modules/adb.nix
@@ -1,15 +1,13 @@
 self:
 {
+  pkgs,
   ...
 }:
 
 {
-  programs.adb.enable = true;
-
-  users.users.gep.extraGroups = [ "adb" ];
-
-  nixpkgs.overlays = [
-    (self.lib.maybeCachePackageOverlay self "android-udev-rules")
-    (self.lib.maybeCachePackageOverlay self "android-tools")
-  ];
+  hm-gep.home.packages =
+    with pkgs;
+    self.lib.maybeCachePackages self [
+      android-tools
+    ];
 }
