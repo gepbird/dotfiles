ed92b5072fdec1d605a65367a018e82e0824dae5
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-01-26 01:13:40 +0100

    lix: 2.93 -> 2.94, update trace-cache patch

--- modules/lix.nix
+++ modules/lix.nix
@@ -6,12 +6,12 @@ self:
 {
   nixpkgs.overlays = [
     (final: prev: {
-      nix = prev.lixPackageSets.lix_2_93.lix.overrideAttrs (o: {
+      nix = prev.lixPackageSets.lix_2_94.lix.overrideAttrs (o: {
         patches = (o.patches or [ ]) ++ [
           (prev.fetchurl {
             name = "trace-cache.patch";
-            url = "https://git.lix.systems/gepbird/lix/compare/2.93.3...2.93.3-trace-cache-4.0.0.patch";
-            hash = "sha256-V09eSBv55rTmDIeVND1ZYF9SJShHIc3cryvvqtfC+4k=";
+            url = "https://git.lix.systems/gepbird/lix/compare/2.94.0...2.94.0-trace-cache-4.0.0.patch";
+            hash = "sha256-fC9TOpUplcIPNXOHXliWaKFK1Tap/+OWVRmfKgBEqXw=";
           })
         ];
       });
