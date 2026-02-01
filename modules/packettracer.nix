25ae5865a30c2ebc210acbcc776f1193492741ae
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-02-01 20:02:09 +0100

    packettracer: ciscoPacketTracer8 -> ciscoPacketTracer9

--- modules/packettracer.nix
+++ modules/packettracer.nix
@@ -7,34 +7,10 @@ self:
 {
   # NOTE: MANUAL INSTALL REQUIRED FOR PACKET TRACER:
   # log into netacad and download packet tracer for ubuntu: https://www.netacad.com/resources/lab-downloads
-  # rename downloaded file to old schema: mv Packet_Tracer822_amd64_signed.deb CiscoPacketTracer822_amd64_signed.deb
-  # add it to the nix store: nix-store --add-fixed sha256 CiscoPacketTracer822_amd64_signed.deb
-  hm-gep.home.packages = with pkgs; [
-    ciscoPacketTracer8
-  ];
-
-  nixpkgs.config.permittedInsecurePackages = [
-    "ciscoPacketTracer8-8.2.2"
-  ];
-
-  nixpkgs.overlays = [
-    # fix text being unreadable and ugly due to half applied dark theme
-    (final: prev: {
-      ciscoPacketTracer8 =
-        self.lib.maybeCacheDerivation
-          "nixpkgs-package-ciscoPacketTracer8-fix-theming-${self.lib.nixpkgsHash self}"
-          (
-            prev.ciscoPacketTracer8.overrideAttrs (o: {
-              nativeBuildInputs = (o.nativeBuildInputs or [ ]) ++ [
-                prev.makeWrapper
-              ];
-              postInstall = (o.postInstall or "") + ''
-                wrapProgram $out/bin/packettracer8 \
-                  --unset QT_QPA_PLATFORMTHEME \
-                  --unset QT_STYLE_OVERRIDE
-              '';
-            })
-          );
-    })
-  ];
+  # add it to the nix store: `nix-store --add-fixed sha256 CiscoPacketTracer_900_Ubuntu_64bit.deb`
+  hm-gep.home.packages =
+    with pkgs;
+    self.lib.maybeCachePackages self [
+      ciscoPacketTracer9
+    ];
 }
