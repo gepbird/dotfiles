c43e2be96604045bca8ef4c4a16e97b585ab5d8c
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-03-03 12:23:04 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Added input 'nixpkgs-patch-packet-tracer-fix-mime':
        'https://github.com/NixOS/nixpkgs/pull/496181.diff?narHash=sha256-1QW2zsDrQ2bUWW%2BhduD1pOCBKz0XgjwR7yQbPUBG8YA%3D'

--- flake.nix
+++ flake.nix
@@ -18,6 +18,10 @@
       url = "https://github.com/NixOS/nixpkgs/pull/494721.diff";
       flake = false;
     };
+    nixpkgs-patch-packet-tracer-fix-mime = {
+      url = "https://github.com/NixOS/nixpkgs/pull/496181.diff";
+      flake = false;
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "";
