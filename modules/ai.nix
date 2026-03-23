bd310249a7dd3b40c334d80c67ae8c0f3671dd33
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-03-23 20:29:19 +0100

    ai: drop Gemini API key

--- modules/ai.nix
+++ modules/ai.nix
@@ -8,12 +8,10 @@ self:
 
 {
   sops.secrets = {
-    "gep/ai-api-keys/gemini".owner = config.users.users.gep.name;
     "gep/ai-api-keys/openrouter".owner = config.users.users.gep.name;
   };
 
   hm-gep.home.sessionVariables = lib.optionalAttrs config.enableSecrets {
-    GEMINI_API_KEY = "$(cat ${config.secrets.gep.ai-api-keys.gemini})";
     OPENROUTER_API_KEY = "$(cat ${config.secrets.gep.ai-api-keys.openrouter})";
   };
 }
