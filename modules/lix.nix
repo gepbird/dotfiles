7eed1277dd94739d83b75fd61e318829260d3166
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-04-04 22:01:16 +0200

    lix: 2.94.0 -> 2.95.1

--- modules/lix.nix
+++ modules/lix.nix
@@ -6,12 +6,12 @@ self:
 {
   nixpkgs.overlays = [
     (final: prev: {
-      nix = prev.lixPackageSets.lix_2_94.lix.overrideAttrs (o: {
+      nix = prev.lixPackageSets.lix_2_95.lix.overrideAttrs (o: {
         patches = (o.patches or [ ]) ++ [
           (prev.fetchurl {
             name = "trace-cache.patch";
-            url = "https://git.lix.systems/gepbird/lix/compare/2.94.0...2.94.0-trace-cache-4.0.0.patch";
-            hash = "sha256-fC9TOpUplcIPNXOHXliWaKFK1Tap/+OWVRmfKgBEqXw=";
+            url = "https://git.lix.systems/gepbird/lix/compare/2.95.1...2.95.1-trace-cache-4.0.2.patch";
+            hash = "sha256-oheoHYC8SlfOo2ltymnKnGb5YOamE8nWUq73UaU7x2o=";
           })
         ];
       });
