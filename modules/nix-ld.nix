b5c3e015550525c396cbe832fb2a2c36fa524f6e
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-04-08 08:36:20 +0200

    nix-ld: remove unused electron-deps

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -11,27 +11,6 @@ self:
     libraries =
       with pkgs;
       let
-        electron-deps = [
-          glib
-          nss
-          nspr
-          atk
-          cups
-          dbus
-          xorg_sys_opengl
-          gtk3
-          pango
-          cairo
-          xorg.libX11
-          xorg.libXcomposite
-          xorg.libXdamage
-          xorg.libXfixes
-          xorg.libXrandr
-          mesa
-          xorg.libxcb
-          libxkbcommon
-          alsa-lib
-        ];
         python-deps = [
           stdenv.cc.cc.lib
         ];
