d7d97f9e1fadc0114c3471ba6ba56e9cf2e118cf
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-04-12 20:03:58 +0200

    nix-cache: extract from nix module

---
+++ modules/nix-cache.nix
@@ -0,0 +1,15 @@
+self:
+{
+  ...
+}:
+
+{
+  nix.settings = {
+    substituters = [
+      "https://nix-cache.tchfoo.com"
+    ];
+    trusted-public-keys = [
+      "nix-cache.tchfoo.com-1:pWK4l0phRA3bE0CviZodEQ5mWAQYoiuVi2LML+VNtNY="
+    ];
+  };
+}
--- modules/nix.nix
+++ modules/nix.nix
@@ -95,12 +95,6 @@ in
         "nix-command"
         "flakes"
       ];
-      substituters = [
-        "https://nix-cache.tchfoo.com"
-      ];
-      trusted-public-keys = [
-        "nix-cache.tchfoo.com-1:pWK4l0phRA3bE0CviZodEQ5mWAQYoiuVi2LML+VNtNY="
-      ];
       trusted-users = [
         "gep"
       ];
