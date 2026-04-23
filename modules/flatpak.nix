922fc59d255ad9151965773e0488af873080f040
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-04-23 15:27:54 +0200

    flatpak: add flathub

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,8 +1,17 @@
 self:
 {
+  pkgs,
   ...
 }:
 
 {
   services.flatpak.enable = true;
+
+  environment.etc = {
+    "flatpak/remotes.d/flathub.flatpakrepo".source = pkgs.fetchurl {
+      url = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      # Let this run once and you will get the hash as an error.
+      hash = "sha256-M3HdJQ5h2eFjNjAHP+/aFTzUQm9y9K+gwzc64uj+oDo=";
+    };
+  };
 }
