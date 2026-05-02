3847dd01e4aefad8eda3b98c4b39831d11e44482
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-05-02 21:06:00 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'flake-parts':
        'github:hercules-ci/flake-parts/3107b77cd68437b9a76194f0f7f9c55f2329ca5b' (2026-04-01)
      → 'github:hercules-ci/flake-parts/5250617bffd85403b14dbf43c3870e7f255d2c16' (2026-05-01)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/1c3fe55ad329cbcb28471bb30f05c9827f724c76' (2026-04-27)
      → 'github:NixOS/nixpkgs/15f4ee454b1dce334612fa6843b3e05cf546efab' (2026-04-30)
    • Added input 'nixpkgs-patch-wireshark-fix-hash':
        'https://github.com/NixOS/nixpkgs/pull/515269.diff?narHash=sha256-aEOXV6DEnFFN/cLQhMvDEctk2ComPt%2B4xQa4qxGSFnA%3D'
    • Updated input 'nur':
        'github:nix-community/NUR/5c632bf603720f8600bae3a9e4fdd0920ace9cb3' (2026-05-01)
      → 'github:nix-community/NUR/b5410cf1d4c7c5db2bc7ba1cc8e8d19e487f9ee0' (2026-05-02)

--- flake.nix
+++ flake.nix
@@ -18,6 +18,10 @@
       url = "https://github.com/NixOS/nixpkgs/pull/496181.diff";
       flake = false;
     };
+    nixpkgs-patch-wireshark-fix-hash = {
+      url = "https://github.com/NixOS/nixpkgs/pull/515269.diff";
+      flake = false;
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "";
