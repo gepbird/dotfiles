5ad449a5ed80cbdcd3ef5c07406bbd078a8219ca
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-05-21 15:17:21 +0200

    discord: fix not opening with OpenASAR

--- flake.nix
+++ flake.nix
@@ -18,6 +18,10 @@
       url = "https://github.com/NixOS/nixpkgs/pull/496181.diff";
       flake = false;
     };
+    nixpkgs-patch-fix-discord-openasar = {
+      url = "https://github.com/NixOS/nixpkgs/pull/520078.diff";
+      flake = false;
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "";
