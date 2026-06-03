956d004da876ff327ce7bdb257299d80d4126f62
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-06-03 15:18:18 +0200

    bitwarden: allow insecure electron-39

--- modules/bitwarden.nix
+++ modules/bitwarden.nix
@@ -5,6 +5,11 @@ self:
 }:
 
 {
+  # TODO: remove after https://github.com/NixOS/nixpkgs/issues/526914
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-39.8.10"
+  ];
+
   hm-gep.home.packages =
     with pkgs;
     self.lib.maybeCachePackages self [
