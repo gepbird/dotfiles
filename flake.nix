2b5eeaa687327a689e50be5937c6b7c3c7cbed46
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-06-20 15:56:27 +0200

    bottom: 0.12.3 -> 0.13.0
    Flake lock file updates:
    
    • Added input 'nixpkgs-patch-bottom-0-13-0':
        'https://github.com/NixOS/nixpkgs/pull/533549.diff?narHash=sha256-y%2B%2BtrNDnYYdmdCzRjp6WPdLj0AlIGxVepinrE1YIXNk%3D'

--- flake.nix
+++ flake.nix
@@ -18,6 +18,10 @@
       url = "https://github.com/NixOS/nixpkgs/pull/496181.diff";
       flake = false;
     };
+    nixpkgs-patch-bottom-0-13-0 = {
+      url = "https://github.com/NixOS/nixpkgs/pull/533549.diff";
+      flake = false;
+    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "";
