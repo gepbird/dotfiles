81b6a4d2b265f422b458cd0fb3b761a9225536a9
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-07-29 10:14:02 +0200

    hm: remove workaround for Unit tray.target not found

--- modules/hm.nix
+++ modules/hm.nix
@@ -25,15 +25,6 @@ self:
 
     users.gep = {
       home.stateVersion = config.system.stateVersion;
-
-      # TODO: remove when fixed: https://github.com/nix-community/home-manager/issues/2064
-      # workaround for 'Failed to restart flameshot.service: Unit tray.target not found.'
-      systemd.user.targets.tray = {
-        Unit = {
-          Description = "Home Manager System Tray";
-          Requires = [ "graphical-session-pre.target" ];
-        };
-      };
     };
   };
 }
