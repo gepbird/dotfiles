3fced922abcbcdf613b468e20e50799ebe630669
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-08-22 17:53:29 +0200

    flutter: drop license removal
    While it still has a top level LICENSE file, it doesn't collide with
    anything used in this config.

--- modules/flutter.nix
+++ modules/flutter.nix
@@ -4,13 +4,10 @@ self:
   ...
 }:
 
-let
-  flutter = self.lib.maybeCacheDerivation "nixpkgs-package-flutter-without-license-${self.lib.nixpkgsHash self}" (
-    self.lib.removeLicense pkgs pkgs.flutter
-  );
-in
 {
-  hm-gep.home.packages = [
-    flutter
-  ];
+  hm-gep.home.packages =
+    with pkgs;
+    self.lib.maybeCachePackages self [
+      flutter
+    ];
 }
