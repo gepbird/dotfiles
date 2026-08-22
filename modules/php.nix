98dc6a6054facc7cb8a88a86305674061cacf6b2
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-08-22 17:44:26 +0200

    php: drop license removal
    It no longer provides a top level LICENSE file.

--- modules/php.nix
+++ modules/php.nix
@@ -4,19 +4,12 @@ self:
   ...
 }:
 
-let
-  composer = self.lib.maybeCacheDerivation "nixpkgs-package-composer-without-license-${self.lib.nixpkgsHash self}" (
-    self.lib.removeLicense pkgs pkgs.phpPackages.composer
-  );
-in
 {
   hm-gep.home.packages =
     with pkgs;
     self.lib.maybeCachePackages self [
-      phpactor
       php
-    ]
-    ++ [
-      composer
+      phpPackages.composer
+      phpactor
     ];
 }
